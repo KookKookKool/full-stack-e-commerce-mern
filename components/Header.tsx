4215b61a2f48740cd946b79331bd02b21a99c46b
KookKookKool <sorakom.aunglan@gmail.com>
2025-06-01 19:54:28 +0700

    feat(header): implement responsive header with mobile and sidebar menus

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Container from "./Container";
 
-const Header = () => {
+import HeaderMenu from "./HeaderMenu";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+
+
+const Header = async () => {
+
   return (
-    <div>Header</div>
-  )
-}
+    <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
+      <Container className="flex items-center justify-between text-lightColor">
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          <MobileMenu />
+          <Logo />
+        </div>
+        <HeaderMenu />
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
 
-export default Header
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
